174b38029595dbe091daddea58c259640ebfbc53
louradoua <alou2000.78@gmail.com>
2021-05-11 10:34:43 +0200

    Added images

--- src/com/eb3/Window.java
+++ src/com/eb3/Window.java
@@ -1,11 +1,37 @@
 package com.eb3;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
 
     public Window(String s){
         super(s);
+        this.setSize(600,500);
+        this.setLayout(new BorderLayout());
+
+        Init_buttons();
+
+        Init_Image();
+
         setVisible(true);
+    }
+
+    public void Init_buttons(){
+
+
+    }
+
+
+    public void Init_Image(){
+        // URL de l'image
+        String imgUrl="sncf.png";	// par exemple
+        ImageIcon icone = new ImageIcon(imgUrl);
+
+        // Création d'un JLabel avec un alignement gauche
+        JLabel image = new JLabel(icone, JLabel.CENTER);
+        image.setBounds(0,-30,200,200);	// par exemple
+        //ajoute le JLabel a la JFrame
+        getContentPane().add(image);
     }
 }
